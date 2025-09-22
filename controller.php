f96ff16dd4a9b3364e9dabafb01d4fc0d051667a
Anaël <anael@barodine.fr>
2025-09-22 13:41:17 +0200

    feat: Màj du chargement des variables d'environnement.

--- controller.php
+++ controller.php
@@ -10,10 +10,13 @@
 ########################################################################################################################
 # Chargement des variables d'environnement                                                                             #
 ########################################################################################################################
-if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/../secrets.php")) {
-    require $_SERVER['DOCUMENT_ROOT'] . "/../secrets.php";
-} elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . "/secrets.php")) {
-    require $_SERVER['DOCUMENT_ROOT'] . "/secrets.php";
+// Si la variable d'environnement FROM_FILE est absente, c'est qu'on a besoin de charger depuis le fichier.
+if (getenv("FROM_FILE") === false || getenv("FROM_FILE") == 1) {
+    foreach (parse_ini_file($_SERVER['DOCUMENT_ROOT'] . "/../.env") as $name => $value) {
+        putenv("$name=$value");
+    }
+} else {
+    putenv("FROM_FILE=0");
 }
 
 ########################################################################################################################
