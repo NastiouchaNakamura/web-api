bb8356f065aec0dd99258792e72656f01b646578
Anaël <anael@barodine.fr>
2025-12-11 22:56:25 +0100

    fix: Réparation de l'outil nb aléatoire

--- random/index.php
+++ random/index.php
@@ -3,46 +3,54 @@ require $_SERVER["DOCUMENT_ROOT"] . "/controller.php";
 
 use App\Model\UserError;
 use App\Model\ServerError;
-use App\Model\University\Building;
 use App\Response\RestResponse;
 
 try {
-    if (isset($_GET["seed"]) and isset($_GET["min"]) and isset($_GET["max"])) {
-        // Vérification des paramètres...
-        if (!is_numeric($_GET["min"])) {
-            echo RestResponse::get(400, UserError::new("Invalid parameter: 'min' must be an integer value"));
-        } elseif (!is_numeric($_GET["max"])) {
-            echo RestResponse::get(400, UserError::new("Invalid parameter: 'max' must be an integer value"));
-        } else {
-            srand(intval(hash("sha256", $_GET["seed"]), 16));
-            $min = intval($_GET["min"]);
-            $max = intval($_GET["max"]);
-
-            if ($min > $max) {
-                echo RestResponse::get(400, UserError::new("Invalid parameter: 'max' must be greater than 'min'"));
-            }
-
-            if (isset($_GET["n"])) {
-                if (!is_numeric($_GET["n"]) or intval($_GET["n"]) <= 0) {
-                    echo RestResponse::get(400, UserError::new("Invalid parameter: 'n' must be a positive integer value"));
-                } else {
-                    $n = intval($_GET["n"]);
-                }
-            } else {
-                $n = 1;
-            }
-
-            $random = array();
-
-            for ($i = 0; $i < $n; $i++) {
-                $random[] = rand($min, $max);
-            }
-
-            echo RestResponse::get(200, $random);
-        }
-    } else {
+    // Vérifications des paramètres
+    
+    if (!isset($_GET["seed"]) || !isset($_GET["min"]) || !isset($_GET["max"])) {
         echo RestResponse::get(400, UserError::new("Missing one or more necessary parameters : string 'seed' must be provided, integer 'min' must be provided and integer 'max' must be provided"));
+        exit();
     }
+
+    if (!is_numeric($_GET["min"])) {
+        echo RestResponse::get(400, UserError::new("Invalid parameter: 'min' must be an integer value"));
+        exit();
+    }
+    
+    if (!is_numeric($_GET["max"])) {
+        echo RestResponse::get(400, UserError::new("Invalid parameter: 'max' must be an integer value"));
+        exit();
+    }
+
+    $min = intval($_GET["min"]);
+    $max = intval($_GET["max"]);
+    if ($min > $max) {
+        echo RestResponse::get(400, UserError::new("Invalid parameter: 'max' must be greater than 'min'"));
+        exit();
+    }
+
+    if (isset($_GET["n"]) && (!is_numeric($_GET["n"]) or intval($_GET["n"]) <= 0)) {
+        echo RestResponse::get(400, UserError::new("Invalid parameter: 'n' must be a positive integer value"));
+        exit();
+    }
+
+    if (isset($_GET["n"]) && intval($_GET["n"]) > 1000) {
+        echo RestResponse::get(400, UserError::new("Invalid parameter: Maximum value count is 1000 per request"));
+        exit();
+    }
+    
+    // Opérations
+
+    srand(intval(hash("sha256", $_GET["seed"]), 16));
+    $n = isset($_GET["n"]) ? intval($_GET["n"]) : 1;
+
+    $random = array();
+    for ($i = 0; $i < $n; $i++) {
+        $random[] = rand($min, $max);
+    }
+
+    echo RestResponse::get(200, $random);
 } catch (Exception $exception) {
     echo RestResponse::get(500, ServerError::new($exception));
 }
--- random/readme.md
+++ random/readme.md
@@ -3,12 +3,12 @@
 Gives a certain number of pseudorandom integers from a given seed.
 
 #### Parameters
-| Name   | Type    | Description                                                                |
-|--------|---------|----------------------------------------------------------------------------|
-| `seed` | string  | Seed used for pseudorandom number generation.                              |
-| `min`  | integer | Smallest possible value of generated pseudorandom integers (included).     |
-| `max`  | integer | Largest possible value of generated pseudorandom integers (included).      |
-| `n`    | integer | (optional) The number of pseudorandom integers to generate, defaults to 1. |
+| Name   | Type    | Description                                                                                                                      |
+|--------|---------|----------------------------------------------------------------------------------------------------------------------------------|
+| `seed` | string  | Seed used for pseudorandom number generation.                                                                                    |
+| `min`  | integer | Smallest possible value of generated pseudorandom integers (included). Must be smaller than `max`.                               |
+| `max`  | integer | Largest possible value of generated pseudorandom integers (included). Must be greater than `min`.                                |
+| `n`    | integer | (optional) The number of pseudorandom integers to generate, defaults to 1. Must be positive. Limited to 1000 at most per request |
 
 #### Exceptions
 
@@ -23,19 +23,8 @@ No exception.
   },
   "data": {
     "type": "array",
-    "size": 10,
-    "data": [
-      "10",
-      "euk",
-      "iut",
-      "llsh",
-      "osuc",
-      "poly",
-      "res",
-      "ru",
-      "st",
-      "univ"
-    ]
+    "size": 1,
+    "values": [4, 5, 9]
   }
 }
 ```
