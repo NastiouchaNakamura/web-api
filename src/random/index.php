7dfe448ce99b0d0de444fdf73922e40ae2272a72
Anaël <anael@barodine.fr>
2026-01-07 20:53:52 +0100

    feat: Description et màj outil random

--- src/random/index.php
+++ src/random/index.php
@@ -6,28 +6,34 @@ use App\Model\ServerError;
 use App\Response\RestResponse;
 
 try {
-    // Vérifications des paramètres
-    if (!isset($_GET["seed"]) || !isset($_GET["min"]) || !isset($_GET["max"]))
-        RestResponse::set(400, UserError::new("Missing one or more necessary parameters : string 'seed' must be provided, integer 'min' must be provided and integer 'max' must be provided"));
-    if (!is_numeric($_GET["min"]))
-        RestResponse::set(400, UserError::new("Invalid parameter: 'min' must be an integer value"));
-    if (!is_numeric($_GET["max"]))
-        RestResponse::set(400, UserError::new("Invalid parameter: 'max' must be an integer value"));
-    if (intval($_GET["min"]) > intval($_GET["max"]))
-        RestResponse::set(400, UserError::new("Invalid parameter: 'max' must be greater than 'min'"));
-    if (isset($_GET["n"]) && (!is_numeric($_GET["n"]) or intval($_GET["n"]) <= 0))
-        RestResponse::set(400, UserError::new("Invalid parameter: 'n' must be a positive integer value"));
-    if (isset($_GET["n"]) && intval($_GET["n"]) > 1000)
-        RestResponse::set(400, UserError::new("Invalid parameter: Maximum value count is 1000 per request"));
-    
-    // Opérations
-    srand(intval(hash("sha256", $_GET["seed"]), 16));
-    $n = isset($_GET["n"]) ? intval($_GET["n"]) : 1;
-    $random = array();
-    for ($i = 0; $i < $n; $i++)
-        $random[] = rand(intval($_GET["min"]), intval($_GET["max"]));
+    // Méthode GET
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        // Vérifications des paramètres
+        if (!isset($_GET["seed"]) || !isset($_GET["min"]) || !isset($_GET["max"]))
+            RestResponse::set(400, UserError::new("Missing one or more necessary parameters : string 'seed' must be provided, integer 'min' must be provided and integer 'max' must be provided"));
+        if (!is_numeric($_GET["min"]))
+            RestResponse::set(400, UserError::new("Invalid parameter: 'min' must be an integer value"));
+        if (!is_numeric($_GET["max"]))
+            RestResponse::set(400, UserError::new("Invalid parameter: 'max' must be an integer value"));
+        if (intval($_GET["min"]) > intval($_GET["max"]))
+            RestResponse::set(400, UserError::new("Invalid parameter: 'max' must be greater than 'min'"));
+        if (isset($_GET["n"]) && (!is_numeric($_GET["n"]) or intval($_GET["n"]) <= 0))
+            RestResponse::set(400, UserError::new("Invalid parameter: 'n' must be a positive integer value"));
+        if (isset($_GET["n"]) && intval($_GET["n"]) > 1000)
+            RestResponse::set(400, UserError::new("Invalid parameter: Maximum value count is 1000 per request"));
+        
+        // Opérations
+        srand(intval(hash("sha256", $_GET["seed"]), 16));
+        $n = isset($_GET["n"]) ? intval($_GET["n"]) : 1;
+        $random = array();
+        for ($i = 0; $i < $n; $i++)
+            $random[] = rand(intval($_GET["min"]), intval($_GET["max"]));
 
-    RestResponse::set(200, $random);
+        RestResponse::set(200, $random);
+        
+    } else {
+        RestResponse::set(405, UserError::new("Method " . $_SERVER['REQUEST_METHOD'] . " is not allowed"));
+    }
 } catch (Exception $exception) {
     RestResponse::set(500, ServerError::new($exception));
 }
